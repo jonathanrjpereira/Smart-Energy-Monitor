a8c84123a90d4087c24214ac77be364b755ac1e7
Jonathan Pereira <jonathanrjpereira@gmail.com>
2017-10-13 21:23:58 +0530

    ACS712 Hardware Test
    Measuring AC current using ACS712 on an Arduino

---
+++ Hardware Tests/ACS712 Current Sensor/Arduino_220VAC/Arduino_220VAC.ino
@@ -0,0 +1,59 @@
+/*
+Measuring AC Current Using ACS712
+*/
+const int sensorIn = A0;
+int mVperAmp = 66; // use 100 for 20A Module and 66 for 30A Module
+
+
+double Voltage = 0;
+double VRMS = 0;
+double AmpsRMS = 0;
+double Amp_Offset = 0.13;
+
+
+
+void setup(){ 
+ Serial.begin(9600);
+}
+
+void loop(){
+ 
+ 
+ Voltage = getVPP();
+ VRMS = (Voltage/2.0) *0.707; 
+ AmpsRMS = (VRMS * 1000)/mVperAmp;
+ Serial.print(AmpsRMS - Amp_Offset);
+ Serial.println(" Amps RMS");
+
+}
+
+float getVPP()
+{
+  float result;
+  
+  int readValue;             //value read from the sensor
+  int maxValue = 0;          // store max value here
+  int minValue = 1024;          // store min value here
+  
+   uint32_t start_time = millis();
+   while((millis()-start_time) < 1000) //sample for 1 Sec
+   {
+       readValue = analogRead(sensorIn);
+       // see if you have a new maxValue
+       if (readValue > maxValue) 
+       {
+           /*record the maximum sensor value*/
+           maxValue = readValue;
+       }
+       if (readValue < minValue) 
+       {
+           /*record the maximum sensor value*/
+           minValue = readValue;
+       }
+   }
+   
+   // Subtract min from max
+   result = ((maxValue - minValue) * 5.0)/1024.0;
+      
+   return result;
+ }
