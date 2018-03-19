8a6edc9ee456b701860374a354bcbc89393f1226
jonathanrjpereira <you@example.com>
2018-03-19 08:34:36 +0530

    Arduino RasPi Serial Communication & Data Transfer

---
+++ Software Test/ArduRaspi.py
@@ -0,0 +1,23 @@
+import serial
+import time
+arduino = serial.Serial('/dev/ttyACM0',9600)
+
+
+while 1:
+    line = arduino.readline()
+    data = line.replace('\r\n','')
+    print data
+   # time.sleep(3.5)
+##    data = []
+##    data.append(line.replace('\r\n',''))
+##    #print data
+##
+##    currDiff =  [float(data[0].replace('Difference in Current:',''))]
+##    #print currDiff
+##
+##    arr = []
+##    for i in xrange(0,5):
+##        arr.append(currDiff)
+##
+##    print arr
+###    return arr
